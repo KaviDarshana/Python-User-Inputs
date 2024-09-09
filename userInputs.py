bf0f3464e7bacd9f44214ebfe6ffc7d8169ea5df
KaviDarshana <152808959+KaviDarshana@users.noreply.github.com>
2024-09-09 15:33:05 +0530

    Update userInputs.py

--- userInputs.py
+++ userInputs.py
@@ -1,21 +1,21 @@
-# #prints on the same line
-# name = input('Enter your name: ')
-# print(name)
-#
-# #priints on a new line
-# print('What is your name?')
-# name = input()
-# print(name)
-#
-# #Eventhough input is a number type displays as a String
-# x = input('Enter a number: ')
-# print(type(x))
-#
-# #convert to a integer
-# x = int(input('Enter a number: '))
-# print(type(x))
+#prints on the same line
+name = input('Enter your name: ')
+print(name)
 
-#Converting numbers to Strings
+#priints on a new line
+print('What is your name?')
+name = input()
+print(name)
+
+#Eventhough input is a number type displays as a String
+x = input('Enter a number: ')
+print(type(x))
+
+#convert to a integer
+x = int(input('Enter a number: '))
+print(type(x))
+
+Converting numbers to Strings
 y = 10
 print(y)
 print('The number is ' + str(y))
